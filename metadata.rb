f2d798de4c2c99ddaf0b7034c4039292843490f2
cn <chrnicolai@gmail.com>
2013-01-27 22:14:50 +0100

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             "openvpn"
 maintainer       "Christian Nicolai"
 maintainer_email "cn@mycrobase.de"
 license          "Apache 2.0"
-description      ""
+description      "A multi-configuration OpenVPN server cookbook featuring IPv6 support and easy setup of client files."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 
