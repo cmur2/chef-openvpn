75f63d3e4984bf16f9e314323ce1e82b5f52fc5a
Denis Barishev <denz@twiket.com>
2014-04-09 04:15:03 +0700

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cn@mycrobase.de"
 license          "Apache 2.0"
 description      "A multi-configuration OpenVPN server cookbook featuring IPv6 support and easy setup of client files."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "3.1.1"
+version          "3.1.2"
 
 suggests "logrotate" # for openvpn::logrotate
 suggests "apt" # for openvpn::use_community_repos
