30adfae67a7a9b15512d67910cdaf91bf9b84b8a
Sergey Melnik <admin.sa@gmail.com>
2014-04-14 16:52:21 +0400

    Update client.rb
    Remove reload, since we do not need reload every run.

--- recipes/client.rb
+++ recipes/client.rb
@@ -1,9 +1,8 @@
-
 include_recipe "openvpn::common"
 include_recipe "openvpn::config_client"
 include_recipe "openvpn::autopki"
 
 service "openvpn" do
-  action [:enable, :start, :reload]
+  action [:enable, :start]
   reload_command "/etc/init.d/openvpn soft-restart"
 end
