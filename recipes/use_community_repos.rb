1ff457229a265b1fbfdad1057b8d1797ea1f32b3
cn <cn@mycrobase.de>
2017-06-22 19:38:39 +0200

    Update to use new repositories of the community OpenVPN project, old ones get no updates

--- recipes/use_community_repos.rb
+++ recipes/use_community_repos.rb
@@ -4,49 +4,37 @@ flavor = node[:openvpn][:community_repo_flavor]
 case node[:platform]
 when "debian"
   case node[:platform_version].to_i
-  when 5
-    apt_repository "openvpn-lenny" do
-      uri "http://repos.openvpn.net/repos/apt/lenny-#{flavor}"
-      components ["lenny", "main"]
-      key "http://repos.openvpn.net/repos/repo-public.gpg"
-    end
-  when 6
-    apt_repository "openvpn-squeeze" do
-      uri "http://swupdate.openvpn.net/apt"
-      components ["squeeze", "main"]
-      key "https://swupdate.openvpn.net/repos/repo-public.gpg"
-    end
   when 7
     apt_repository "openvpn-wheezy" do
-      uri "http://swupdate.openvpn.net/apt"
+      uri "http://build.openvpn.net/debian/openvpn/release/2.3"
       components ["wheezy", "main"]
+      key "https://swupdate.openvpn.net/repos/repo-public.gpg"
+    end
+  when 8
+    apt_repository "openvpn-jessie" do
+      uri "http://build.openvpn.net/debian/openvpn/release/2.3"
+      components ["jessie", "main"]
       key "https://swupdate.openvpn.net/repos/repo-public.gpg"
     end
   end
 when "ubuntu"
   case node[:platform_version]
-  when "10.04", "10.10", "11.04", "11.10"
-    apt_repository "openvpn-lucid" do
-      uri "http://swupdate.openvpn.net/apt"
-      components ["lucid", "main"]
-      key "https://swupdate.openvpn.net/repos/repo-public.gpg"
-    end
   when "12.04"
     apt_repository "openvpn-precise" do
-      uri "http://swupdate.openvpn.net/apt"
+      uri "http://build.openvpn.net/debian/openvpn/release/2.3"
       components ["precise", "main"]
       key "https://swupdate.openvpn.net/repos/repo-public.gpg"
     end
-  when "13.04"
-    apt_repository "openvpn-raring" do
-      uri "http://swupdate.openvpn.net/apt"
-      components ["raring", "main"]
+  when "14.04"
+    apt_repository "openvpn-trusty" do
+      uri "http://build.openvpn.net/debian/openvpn/release/2.3"
+      components ["trusty", "main"]
       key "https://swupdate.openvpn.net/repos/repo-public.gpg"
     end
-  when "13.10"
-    apt_repository "openvpn-saucy" do
-      uri "http://swupdate.openvpn.net/apt"
-      components ["saucy", "main"]
+  when "16.04"
+    apt_repository "openvpn-xenial" do
+      uri "http://build.openvpn.net/debian/openvpn/release/2.3"
+      components ["xenial", "main"]
       key "https://swupdate.openvpn.net/repos/repo-public.gpg"
     end
   end
